3cba005f820afb070e9efa6e033affaa6842afa8
Ok <arsen.kalviyainen@gmail.com>
2021-10-09 16:33:42 +0300

    gg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonom.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonom.java
@@ -5,6 +5,7 @@ import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -18,71 +19,70 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 @Autonomous
 public class Autonom extends LinearOpMode {
 
+    //creating variables
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotorEx leftMotorFront = null;
     private DcMotorEx rightMotorFront = null;
     private DcMotorEx leftMotorBack = null;
     private DcMotorEx rightMotorBack = null;
 
-    public void Angle(double angleF, char direction){
-        double xR = rightMotorFront.getCurrentPosition();
-        double yL = leftMotorFront.getCurrentPosition();
-        double speed = 0.5;
-        final double R = rightMotorFront.getCurrentPosition();
-        final double L = leftMotorFront.getCurrentPosition();
+    public void Move(double distance, double angle) {
+        double linerSpeed = 0;
+        double currentAngle = 0;
+        double integralSpeed = 0;
+        double integralAngle = 0;
+        double kP = 0.001;
+        double kPA = 0.001;
+        double kI = 0.00001;
+        double kD = 0;
+        do {
+            //proportional component
+            linerSpeed = (distance - (rightMotorFront.getCurrentPosition() + leftMotorFront.getCurrentPosition()) / 2.0) * kP;
+            currentAngle = (angle - (rightMotorFront.getCurrentPosition() - leftMotorFront.getCurrentPosition())/2.0) * kPA;
 
-        while((xR > (R+angleF) && direction == 'R') || (yL > (L+angleF) && direction =='L')){
-            if (direction == 'R') {
-                rightMotorFront.setPower(-speed);
-                leftMotorFront.setPower(speed);
-            }
-            else if (direction == 'L') {
-                rightMotorFront.setPower(speed);
-                leftMotorFront.setPower(-speed);
-            }
-        }
-    }
+            //integral component
+            integralSpeed = (distance - (rightMotorFront.getCurrentPosition() + leftMotorFront.getCurrentPosition()) / 2.0) * kI;
+            integralAngle = (angle - (rightMotorFront.getCurrentPosition() - leftMotorFront.getCurrentPosition())/2.0) * kI;
 
-    public void Move(double distance, double angle, char direction) {
-        double xR = rightMotorFront.getCurrentPosition();
-        double yL = leftMotorFront.getCurrentPosition();
-        double speed_R = 0;
-        double speed_L = 0;
-        double kf = 1;
-        speed_R = (distance - xR) * kf;
-        speed_L = (distance - yL) * kf;
-        rightMotorFront.setPower(speed_R);
-        leftMotorFront.setPower(speed_L);
-        Angle(angle,direction);
-    }
+            //differential component
+
+
+            rightMotorFront.setPower(linerSpeed + currentAngle + integralSpeed);
+            leftMotorFront.setPower(linerSpeed - currentAngle + integralAngle);
+
+            telemetry.addData("speed", linerSpeed);
+            telemetry.addData("Rrrrr", rightMotorFront.getCurrentPosition());
+            telemetry.addData("Lllll", leftMotorFront.getCurrentPosition());
+            telemetry.update();
+        } while(Math.abs(linerSpeed) > 0.05 || Math.abs(currentAngle) > 0.05);
+   }
 
     public void Move(double dist){
-        Move(dist, 0, 'o');
+        Move(dist, 0);
     }
-
-    AllFunction fun = new AllFunction();
 
     @Override
     public void runOpMode() {
 
+        //init
         rightMotorFront = hardwareMap.get(DcMotorEx.class, "R1");
         rightMotorBack = hardwareMap.get(DcMotorEx.class, "R2");
         leftMotorFront = hardwareMap.get(DcMotorEx.class, "L1");
         leftMotorBack = hardwareMap.get(DcMotorEx.class, "L2");
 
+        //set direction
         rightMotorFront.setDirection(DcMotorEx.Direction.FORWARD);
         leftMotorFront.setDirection(DcMotorEx.Direction.REVERSE);
         rightMotorBack.setDirection(DcMotorEx.Direction.REVERSE);
         leftMotorBack.setDirection(DcMotorEx.Direction.FORWARD);
 
+        //zeroing encoders
+        for(DcMotorEx dcMotorEx: hardwareMap.getAll(DcMotorEx.class)) {
+            dcMotorEx.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            dcMotorEx.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
+
         waitForStart();
-
-        telemetry.addData("Status", "Initialized");
-
-        Move(0,200, 'R');
-        Move(1000);
-        telemetry.addData("Rrrrr", rightMotorFront.getCurrentPosition());
-        telemetry.addData("Lllll", leftMotorFront.getCurrentPosition());
-        telemetry.update();
+        Move(0, 200);
     }
 }
