178195c0c97101f99b288abd4b4fee9a5540cd2f
december25 <zhora.gorelow@yandex.ru>
2021-10-20 23:39:13 +0300

    for All

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ButtonActivatedModes/Duck.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ButtonActivatedModes/Duck.java
@@ -1,16 +1,13 @@
-package org.firstinspires.ftc.teamcode.opmodes;
+package org.firstinspires.ftc.teamcode.opmodes.ButtonActivatedModes;
 
 
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Gamepad;
 
-public class Duck {
+public class Duck implements ButtonActivated {
     private DcMotor duckMotor = null;
     private LinearOpMode linearOpMode = null;
-    private Gamepad gamepad1 = linearOpMode.gamepad1;
-    private int i = 0;
 
     public Duck(LinearOpMode linearOpMode){
         this.linearOpMode = linearOpMode;
@@ -20,7 +17,11 @@ public class Duck {
         duckMotor = linearOpMode.hardwareMap.get(DcMotor.class, "DuckMotor");
     }
 
-    public void spin(boolean doSpin){
+    public void activity(boolean n){
+        DuckSpin(n);
+    }
+
+    public void DuckSpin(boolean doSpin){
         if(doSpin) duckMotor.setPower(Range.clip(1,-1,1));
         else duckMotor.setPower(Range.clip(0,-1,1));
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ButtonActivatedModes/Manipulator.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.opmodes.ButtonActivatedModes;
+
+public class Manipulator implements ButtonActivated {
+    @Override
+    public void activity(boolean n) {
+
+    }
+}
