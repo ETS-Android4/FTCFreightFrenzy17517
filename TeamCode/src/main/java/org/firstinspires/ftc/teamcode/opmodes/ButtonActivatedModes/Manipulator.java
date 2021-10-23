60e97aaeb71620b65a6f0871add6872035fb971e
oaleksander <medvedev.asm@outlook.com>
2021-10-23 19:32:27 +0300

    update Manipulator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ButtonActivatedModes/Manipulator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ButtonActivatedModes/Manipulator.java
@@ -1,23 +1,38 @@
 package org.firstinspires.ftc.teamcode.opmodes.ButtonActivatedModes;
 
+import static org.firstinspires.ftc.teamcode.VariablesDashboard.ManipulatorConfig.*;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Manipulator {
+
+    /*
+    DigitalChannel endSwitchUp;
+    DigitalChannel endSwitchDown;*/
+    private Servo liftMotor = null;
+
+    enum ManipulatorPosition {
+        UP, DOWN, CENTRAL
+    }
+
     private LinearOpMode linearOpMode = null;
     public Manipulator(LinearOpMode linearOpMode){
         this.linearOpMode = linearOpMode;
     }
 
-    private DcMotorEx liftMotor = null;
-
     public void init(){
-        liftMotor = linearOpMode.hardwareMap.get(DcMotorEx.class, "UpDown");
+        liftMotor = linearOpMode.hardwareMap.get(Servo.class, "UpDown");
+    }
 
-        for(DcMotorEx dcMotorEx: linearOpMode.hardwareMap.getAll(DcMotorEx.class)) {
-            dcMotorEx.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            dcMotorEx.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    public void MoveServo(ManipulatorPosition manipulatorPosition) {
+        switch (manipulatorPosition){
+            case UP: liftMotor.setPosition(manipulatorUp);
+            case DOWN: liftMotor.setPosition(manipulatorDown);
+            case CENTRAL: liftMotor.setPosition(manipulatorCentral);
         }
     }
 }
