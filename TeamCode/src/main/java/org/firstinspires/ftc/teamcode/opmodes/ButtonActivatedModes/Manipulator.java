3e9853a32d4f480c77d7cc73123ccd64e206a76f
december25 <zhora.gorelow@yandex.ru>
2021-10-23 18:36:05 +0300

    for All

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ButtonActivatedModes/Manipulator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ButtonActivatedModes/Manipulator.java
@@ -1,8 +1,23 @@
 package org.firstinspires.ftc.teamcode.opmodes.ButtonActivatedModes;
 
-public class Manipulator implements ButtonActivated {
-    @Override
-    public void activity(boolean n) {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+public class Manipulator {
+    private LinearOpMode linearOpMode = null;
+    public Manipulator(LinearOpMode linearOpMode){
+        this.linearOpMode = linearOpMode;
+    }
+
+    private DcMotorEx liftMotor = null;
+    void init(){
+        liftMotor = linearOpMode.hardwareMap.get(DcMotorEx.class, "UpDown");
+
+        for(DcMotorEx dcMotorEx: linearOpMode.hardwareMap.getAll(DcMotorEx.class)) {
+            dcMotorEx.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            dcMotorEx.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
     }
 }
+
