76d59b9079f05b7a10544071a159651e5adc6ed4
Ok <arsen.kalviyainen@gmail.com>
2021-11-13 16:31:25 +0300

    Final today

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Movement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Movement.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.robot;
 
 import static org.firstinspires.ftc.teamcode.VariablesDashboard.MovementConfig.*;
 import static java.lang.Math.PI;
