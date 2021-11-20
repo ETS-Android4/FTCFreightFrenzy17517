0f4eae5bb8256bf81e1c98342dc93655e0da4b53
december25 <zhora.gorelow@yandex.ru>
2021-11-20 19:38:54 +0300

    for Arsen

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Arsen.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Arsen.java
@@ -4,12 +4,12 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-// 0 expentoin Brush
-public class Brush implements RobotModule{
+
+public class Arsen implements RobotModule{
     private DcMotorEx brushMotor = null;
     private LinearOpMode linearOpMode = null;
 
-    public Brush(LinearOpMode linearOpMode) {
+    public Arsen(LinearOpMode linearOpMode) {
         this.linearOpMode = linearOpMode;
     }
     public void init() {
@@ -19,17 +19,13 @@ public class Brush implements RobotModule{
         brushMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         brushMotor.setDirection(DcMotorEx.Direction.REVERSE);
     }
-    public boolean bra = false;
-    public void brushMotorMove(boolean bra){
-        this.bra = bra;
+    public boolean intake = false;
+    public void brushMotorMove(boolean intake){
+        this.intake = intake;
     }
     public void update(){
-        if(bra){
-            brushMotor.setPower(1);
-        }
-        else{
-            brushMotor.setPower(0);
-        }
+        if(intake){ brushMotor.setPower(1); }
+        else{ brushMotor.setPower(0); }
     }
     public boolean line(){
         return true;
