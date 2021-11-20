671c82dce1a2f8a90145504f77aed2c97d1c96b4
Ok <arsen.kalviyainen@gmail.com>
2021-11-20 16:04:59 +0300

    1 day above competition 4

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpOneGamepad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpOneGamepad.java
@@ -35,7 +35,7 @@ public class TeleOpOneGamepad extends LinearOpMode {
 
         waitForStart();
         while (opModeIsActive()) {
-            robotModules.movement.setMotorPowers(-gamepad1.left_stick_y * robotSpeed, gamepad1.right_stick_x * robotSpeed);
+            RobotModules.movement.setMotorPowers(-gamepad1.left_stick_y * robotSpeed, gamepad1.right_stick_x * robotSpeed);
             duck_function.activate();
             if(gamepad1.triangle) RobotModules.elevator.MoveServoForElevator(true);
             if(gamepad1.cross)RobotModules.elevator.MoveServoForElevator(false);
