9de8ae5dc6167cc66f0901ab9016bf79dd0852b9
Ok <arsen.kalviyainen@gmail.com>
2021-12-17 16:01:54 +0300

    1 day above competition 3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Brush.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Brush.java
@@ -13,7 +13,7 @@ public class Brush implements RobotModule{
     private DcMotorEx brushMotor = null;
     private LinearOpMode linearOpMode = null;
     private DistanceSensor distance = null;
-    private DcMotorEx ledMotor = null;
+    public DcMotorEx ledMotor = null;
 
     public Brush(LinearOpMode linearOpMode) {
         this.linearOpMode = linearOpMode;
@@ -35,12 +35,14 @@ public class Brush implements RobotModule{
     public void update(){
         if(intake && distance.getDistance(DistanceUnit.CM) > 8){
             brushMotor.setPower(1);
-            ledMotor.setPower(1);
+            ledMotor.setPower(0);
         }
         else if (intake && distance.getDistance(DistanceUnit.CM ) < 8){
             brushMotor.setPower(-1);
+            ledMotor.setPower(1);
         } else {
             brushMotor.setPower(0);
+            ledMotor.setPower(0);
         }
     }
     public void updateTele(boolean t1){
